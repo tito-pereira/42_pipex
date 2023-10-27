fdd24eaebe701ac77b2de616482ab9e400d8da7c
Tito Alexandre Barbosa Pereira <tab.pereira00@gmail.com>
2023-10-27 16:45:50 +0100

    m

--- m_main.c
+++ m_main.c
@@ -6,7 +6,7 @@
 /*   By: tibarbos <tibarbos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 14:34:00 by tibarbos          #+#    #+#             */
-/*   Updated: 2023/10/26 14:44:15 by tibarbos         ###   ########.fr       */
+/*   Updated: 2023/10/27 16:45:20 by tibarbos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,26 @@
 
 int	main(int ac, char **av)
 {
+	int	fd1;
+	int	pid;
+	
 	if (ac == 5)
-	{}
+	{
+		if (av[1] == "here_doc")
+		{
+			while (buff[] != LIMITER)
+			{
+				read(1, buff, 1);
+			}
+			av[1] == buff;
+		}
+		fd1 = open(av[1], O_RDONLY);
+		pid = fork();
+		if (pid == 0)
+			do_exec();
+		wait(NULL);
+		close (fd1);
+	}
 	else
-	{}//error message?
+		ft_printf("Please input correct arguments or commands.\n");
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: tibarbos <tibarbos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 14:31:19 by tibarbos          #+#    #+#             */
-/*   Updated: 2023/10/26 16:05:07 by tibarbos         ###   ########.fr       */
+/*   Updated: 2023/10/27 16:42:22 by tibarbos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@
 
 # include <unistd.h> //write, read, fork
 # include <stdlib.h> //malloc, free
+# include <sys/wait.h> //wait, waitpid, getpid, getppid
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h> //open
 # include "./libft/mylib.h"
+
+# define LIMITER "LIM"
+
+//atencao ao mylib/libft porque ha funcoes auxiliares c o mesmo nome
+//se so chamar ft_printf ou getnext na boa, as outras cuidado
 
 #endif
