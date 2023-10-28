7f856e795be485d424fb1650169042814f60dc2f
tito-pereira <tab.pereira00@gmail.com>
2023-10-28 17:17:29 +0200

    m

--- m_main.c
+++ m_main.c
@@ -12,11 +12,58 @@
 
 #include "pipex.h"
 
+void	ft_read(int fd, char *buff)
+{
+	int	ret;
+
+	ret = 1;
+	while (ret != 0)
+	{
+		ret = read(fd, buff, INT_MAX);
+	}
+}
+
+void	two_exec(int *p2, char *buff2)
+{
+	char	*buff3;
+
+	close(p2[0]);
+	buff3 = execve(buff2);
+	write(p2[1], buff3);
+	free(buff2);
+	close(p2[1]);
+}
+
+void	one_exec(int *p1, char *buff1)
+{
+	int	*p2;
+	int	pid2;
+	char	*buff2;
+	char	*buff3;
+
+	close(p1[0]);
+	buff2 = execve(buff1);
+	p2 = malloc(2 * sizeof(int));
+	pipe(p2);
+	pid2 = fork()
+	if (pid2 == 0)
+		two_exec(p2, buff2);
+	wait(NULL);
+	close(p2[1]);
+	ft_read(p2[0], buff3);
+	close(p2[0]);
+	write(p1[1], buff3);
+	close(p1[1]);
+	free(p2);
+}
+
 int	main(int ac, char **av)
 {
-	int	fd1;
-	int	fd2;
-	int	pid;
+	int	in;
+	int	out;
+	int	pid1;
+	int	*p1;
+	char	*buff1;
 	
 	if (ac == 5)
 	{
@@ -28,26 +75,21 @@ int	main(int ac, char **av)
 			}
 			av[1] == buff;
 		}
-		fd1 = open(av[1], O_RDONLY);
-		fd2 = open(av[4], O_RDWR, O_CREAT);
-		pipe({fd2, fd1});
-		//definir se uso flags para append ou para exclude o que ta la dentro
-		//consoante o bonus ou nao
-		/*
-		acho que tou a fazer isto mal, pipe e usado p comunicar entre processos
-		e nao entre files
-		*/
-		pid = fork();
-		/*
-		child process so comeca a correr daqui, nao faz a leitura nem o pipe,
-		mas tem essas variaveis guardadas
-		*/
+		in = open(av[1], O_RDONLY);
+		ft_read(in, buff1);
+		close(in);
+		p1 = malloc(2 * sizeof(int));
+		pipe(p1);
+		pid1 = fork();
 		if (pid == 0)
-			do_exec();
+			do_exec(p1, buff1);
 		wait(NULL);
-		close (fd1);
-		errno;
-		ECHILD;
+		close(p1[1]);
+		read(p1[0]);
+		out = open(av[4], O_RDWR, O_CREAT);
+		write(out);
+		close (out);
+		free(p1);
 	}
 	else
 		ft_printf("Please input correct arguments or commands.\n");
